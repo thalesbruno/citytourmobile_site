0d89c4add94a1a169e91e6260f7424df60ee85f6
Thales Bruno <thalesbrunom@gmail.com>
2014-05-31 18:40:52 -0300

    slide responsivo

--- static/js/functions.js
+++ static/js/functions.js
@@ -8,3 +8,35 @@ function rolar_para(elemento) {
     scrollTop: $(elemento).offset().top
   }, 2000);
 }
+
+/**/
+largura = $(window).width();
+
+if (largura < 400) {
+	for(var i=1; i<=3; i++) {
+		$('#slide'+i).attr({
+			src: "static/images/slide"+i+"-320.png"
+		});
+	}
+}
+else if (largura < 800) {
+	for(var i=1; i<=3; i++) {
+		$('#slide'+i).attr({
+			src: "static/images/slide"+i+"-768.png"
+		});
+	}
+}
+else if (largura < 1100) {
+	for(var i=1; i<=3; i++) {
+		$('#slide'+i).attr({
+			src: "static/images/slide"+i+"-1024.png"
+		});
+	}
+}
+else if (largura < 1300) {
+	for(var i=1; i<=3; i++) {
+		$('#slide'+i).attr({
+			src: "static/images/slide"+i+"-1280.png"
+		});
+	}
+}
